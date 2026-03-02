c8ae13293ff31823c18728247a73d1d3a7e6e56f
Jerry <jerrywbirnbaum@gmail.com>
2026-03-02 12:03:07 -0700

    added react chess ui

--- go-chess-react/src/App.tsx
+++ go-chess-react/src/App.tsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Chessboard } from 'react-chessboard';
+import { Chess } from 'chess.js';
+import { useRef, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const chessGameRef = useRef(new Chess());
+  const chessGame = chessGameRef.current;
+
+  // track the current position of the chess game in state to trigger a re-render of the chessboard
+  const [chessPosition, setChessPosition] = useState(chessGame.fen());
+
+  // make a random "CPU" move
+  function makeRandomMove() {
+    // get all possible moves`
+    const possibleMoves = chessGame.moves();
+
+    // exit if the game is over
+    if (chessGame.isGameOver()) {
+      return;
+    }
+
+    // pick a random move
+    const randomMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+
+    // make the move
+    chessGame.move(randomMove);
+
+    // update the position state
+    setChessPosition(chessGame.fen());
+  }
+
+  // handle piece drop
+  function onPieceDrop({
+    sourceSquare,
+    targetSquare
+  }: PieceDropHandlerArgs) {
+    // type narrow targetSquare potentially being null (e.g. if dropped off board)
+    if (!targetSquare) {
+      return false;
+    }
+
+    // try to make the move according to chess.js logic
+    try {
+      chessGame.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: 'q' // always promote to a queen for example simplicity
+      });
+
+      // update the position state upon successful move to trigger a re-render of the chessboard
+      setChessPosition(chessGame.fen());
+
+      // make random cpu move after a short delay
+      setTimeout(makeRandomMove, 5);
+
+      // return true as the move was successful
+      return true;
+    } catch {
+      // return false as the move was not successful
+      return false;
+    }
+  }
+
+  // set the chessboard options
+  const chessboardOptions = {
+    position: chessPosition,
+    onPieceDrop,
+    id: 'play-vs-random',
+
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Chessboard options={chessboardOptions} />);
 }
+
 
 export default App
