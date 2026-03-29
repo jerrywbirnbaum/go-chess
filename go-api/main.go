84e78f15677fb326030d476ce231c42b26215159
Jerry <jerrywbirnbaum@gmail.com>
2026-03-29 09:53:09 -0600

    add profile tools

--- go-api/main.go
+++ go-api/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "os"
+import (
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+)
 
 type ChessRequest struct {
 	FenString string `json:"fen" binding:"required"`
@@ -11,6 +15,7 @@ type TimerRequest struct {
 }
 
 func main() {
+	go http.ListenAndServe(":8080", nil)
 	board := initBoard()
 	moveGenerator := MoveGenerator{board: &board}
 	chessEngine := ChessEngine{moveGenerator: moveGenerator}
---
+++ profile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+cd go-api && go build -o go-chess . && cd ..
+
+fastchess \
+      -engine cmd="./go-api/go-chess" args="uci" name=GoChess \
+      -engine cmd="./binaries/v7" args="uci" name=Old \
+      -each tc=30+0.3 -openings file=openings.epd format=epd \
+      -rounds 10 -pgnout append=false notation=san nodes=true file=./games/round1.pgn \
+      -resign movecount=60 score=1500 -recover &
+
+FASTCHESS_PID=$!
+
+# Wait for go-chess pprof server to initialize
+sleep 2
+go tool pprof -http=:9090 "http://localhost:8080/debug/pprof/profile?seconds=30" &
+
+wait $FASTCHESS_PID
